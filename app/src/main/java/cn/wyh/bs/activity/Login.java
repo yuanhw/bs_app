df7ed8152ae7fd91bc95cd7317ee9005439f04ed
wyh <13588746259@163.com>
2018-01-16 13:47:45 +0800

    修改

--- app/src/main/java/cn/wyh/bs/activity/Login.java
+++ app/src/main/java/cn/wyh/bs/activity/Login.java
@@ -12,16 +12,9 @@ import android.widget.Toast;
 
 import com.alibaba.fastjson.JSONObject;
 
-import java.io.IOException;
-
 import cn.wyh.bs.R;
 import cn.wyh.bs.common.Global;
 import cn.wyh.bs.entity.User;
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class Login extends BaseActivity {
 
