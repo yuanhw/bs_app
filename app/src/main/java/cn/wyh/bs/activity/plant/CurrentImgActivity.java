a571f34a518f764da7c344c4dc8d6d5525f55284
wyh <13588746259@163.com>
2018-05-31 01:26:35 +0800

    修改地块监控界面

--- app/src/main/java/cn/wyh/bs/activity/plant/CurrentImgActivity.java
+++ app/src/main/java/cn/wyh/bs/activity/plant/CurrentImgActivity.java
@@ -14,6 +14,8 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.List;
 
+import cn.jzvd.JZVideoPlayer;
+import cn.jzvd.JZVideoPlayerStandard;
 import cn.wyh.bs.R;
 import cn.wyh.bs.activity.BaseActivity;
 import cn.wyh.bs.adapter.RecordAdapter;
@@ -25,14 +27,16 @@ import cn.wyh.bs.common.Global;
  */
 
 public class CurrentImgActivity extends BaseActivity {
+    /*
     private String plantId;
     private ImageView[] imgs = new ImageView[3];
+    */
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.current_img);
-        plantId = getIntent().getStringExtra("plantId");
+        //plantId = getIntent().getStringExtra("plantId");
         ImageView back = (ImageView) findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,12 +44,25 @@ public class CurrentImgActivity extends BaseActivity {
                 finish();
             }
         });
+        /*
         imgs[0] = (ImageView) findViewById(R.id.img1);
         imgs[1] = (ImageView) findViewById(R.id.img2);
         imgs[2] = (ImageView) findViewById(R.id.img3);
-        loadData();
+        */
+        //loadData();
+
+        String path = "/tillageVideo/001.mp4";
+        JZVideoPlayerStandard player1 = (JZVideoPlayerStandard) findViewById(R.id.video_player1);
+        player1.setUp(path, JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL, "耕种操作");
+
+        JZVideoPlayerStandard player2 = (JZVideoPlayerStandard) findViewById(R.id.video_player2);
+        player2.setUp(path, JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL, "耕种操作");
+
+        JZVideoPlayerStandard player3 = (JZVideoPlayerStandard) findViewById(R.id.video_player3);
+        player3.setUp(path, JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL, "耕种操作");
     }
 
+    /*
     private void loadData() {
         new Thread(new Runnable() {
             @Override
@@ -69,5 +86,19 @@ public class CurrentImgActivity extends BaseActivity {
                 });
             }
         }).start();
+    }
+    */
+    @Override
+    public void onBackPressed() {
+        if (JZVideoPlayer.backPress()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        JZVideoPlayer.releaseAllVideos();
     }
 }
