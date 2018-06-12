beedf88df1ea03e9386f222f4732eeb4a0c93c43
wangyuanhang <13588746259@163.com>
2018-06-13 00:07:02 +0800

    测试

---
+++ app/src/main/java/cn/wyh/bs/Main.java
@@ -0,0 +1,12 @@
+package cn.wyh.bs;
+
+/**
+ * @author: WangYuanHang
+ * @Description:
+ * @date: 2018/6/130:05
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("hello world!");
+    }
+}
